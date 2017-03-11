e182230170a897df3d124a5a0d3e67d46007dc63
iH8Ra1n <ddwrtuuuu@gmail.com>
2017-03-11 08:57:55 -0500

    Add code to ion.py
    (Added functions into Ion)

--- ion.py
+++ ion.py
@@ -1,4 +1,5 @@
 import os, sys, subprocess, time
+fs = {}
 def hio():
     try:
         io(open('.tmp', 'r').read())
@@ -7,7 +8,7 @@ def hio():
 def io(file):
     x = open(file, 'r').read().split('\n')
     for item in x:
-        print(ion(item))
+        ion(item)
 def partition(alist, indices):
     return [alist[i:j] for i, j in zip([0]+indices, indices+[None])]
 def ion(c):
@@ -15,8 +16,8 @@ def ion(c):
     for item in cc:
         if item == '||':
             if cc[cc.index(item) - 1] == 'print':
-                tmp = cc[cc.index(item) + 1]
-                exec('print(' + tmp + ')')
+                tmp = cc[cc.index(item) + 1:]
+                exec('print(' + ''.join(tmp) + ')')
                 return ''
         elif item == '~':
             if cc[cc.index(item) - 1] == '+':
@@ -62,8 +63,20 @@ def ion(c):
                 args = cc[cc.index(item) + 1]
                 x = input(args)
                 return x
+            elif cc[cc.index(item) - 1] == 'exio':
+                args = cc[cc.index(item) + 1:]
+                try:
+                    exec('ion("' + eval('  '.join(args[0:])) + '")')
+                except:
+                    pass
+            elif cc[cc.index(item) - 1] == 'define':
+                args = cc[cc.index(item) + 1:]
+                fs[args[0]] = '  '.join(args[1:])
             else:
-                return ''.join(list(c)[:-3])
+                if c.split('  ')[0] in fs:
+                    return ion(eval(fs[c.split('  ')[0]]))
+                else:
+                    return ''.join(list(c)[:-3])
         elif item == '=':
             tmp = ion('  '.join(cc[cc.index(item) + 1:]) + '  ~')
             exec('global ' + cc[cc.index(item) - 1] + '; ' + cc[cc.index(item) - 1] + ' = tmp')
